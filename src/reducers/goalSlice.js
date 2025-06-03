a81e75a047e5fa71f8a801b1bdb587fb6ef16774
Jose Melendez <23001327@galileo.edu>
2025-06-03 16:00:09 -0600

    Realizar conexion a backend

--- src/reducers/goalSlice.js
+++ src/reducers/goalSlice.js
@@ -13,6 +13,33 @@ export const goalSlice = createSlice({
         addGoal: (state, action ) => {
             console.log(action.payload);
             state.Value.push(action.payload);
+            fetch('http://localhost:3001/goals/addGoal', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': '123456'
+                },
+                body: JSON.stringify(action.payload)
+            }).catch((err) => {
+                console.log(err);
+            });
+        },
+        initAddGoal: (state, action) => {
+            console.log(action.payload);
+            state.Value.push(action.payload);
+        },
+        removeGoal: (state, action) => {
+            state.Value = state.Value.filter((task) => task._id !== action.payload);
+            fetch('http://localhost:3001/goals/removeGoal'+ action.payload, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': '123456'
+                },
+                body: JSON.stringify(action.payload)
+            }).catch((err) => {
+                console.log(err);
+            });
         }
     }
 })
--- src/reducers/todoSlice.js
+++ src/reducers/todoSlice.js
@@ -13,14 +13,37 @@ export const todoSlice = createSlice({
         addTodo: (state, action ) => {
             console.log(action.payload);
             state.Value.push(action.payload);
+            fetch('http://localhost:3001/tasks/addTask', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': '123456'
+                },
+                body: JSON.stringify(action.payload)
+            }).catch((err) => {
+                console.log(err);
+            });
         },
         initAddTodo: (state, action) => {
             console.log(action.payload);
             state.Value.push(action.payload);
+        },
+        removeTodo: (state, action) => {
+            state.Value = state.Value.filter((task) => task._id !== action.payload);
+            fetch('http://localhost:3001/tasks/removeTask'+ action.payload, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': '123456'
+                },
+                body: JSON.stringify(action.payload)
+            }).catch((err) => {
+                console.log(err);
+            });
         }
     }
 })
 
-export const { addTodo, initAddTodo } = todoSlice.actions
+export const { addTodo, initAddTodo, removeTodo } = todoSlice.actions
 export const selectTools = (state) => state.todos.Value
 export const todoReducer = todoSlice.reducer
